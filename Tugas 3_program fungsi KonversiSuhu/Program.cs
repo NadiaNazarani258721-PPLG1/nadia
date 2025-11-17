6e54ac64c5153150d6ea3e94b732de69226bc76b
nazarani258721-crypto <nazarani258721@student.smkn1kandeman.sch.id>
2025-11-17 13:43:28 +0700

    Add files via upload

---
+++ Tugas 3_program fungsi KonversiSuhu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_3_program_fungsi_KonversiSuhu
+{
+    internal class Program
+    {
+        static int KonversiSuhu(int c)
+        {
+            int rm = (c * 9 / 5) + 32;
+            return rm;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan suhu celcius yang akan dirubah ke fahrenhait  :  ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int hasil = KonversiSuhu(x);
+            Console.WriteLine("nilai hasil konversi suhu dari celcius ke fahrenhait  :  " + hasil + "°F");
+
+
+        }
+    }
+}
+
